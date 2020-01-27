6e28a0a458d579a078f16993428049eb8c8ee108
Swarup Ghosh <codecrafts.cf@icloud.com>
2020-01-27 15:44:56 +0530

    Fix train.sh multiline command

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python3 train_on_tpu.py --tpu_worker "grpc://10.0.27.2:8470" \ 
---class_labels="gs://shrill-anstett-us/C-MS-Celeb-class-labels.txt" \ 
---dataset_path="gs://shrill-anstett-us/C-MS-Celeb-TFRecords" \ 
---tensorboard_path="gs://shrill-anstett-us/C-MS-Celeb-FaceNet_TensorBoard" \ 
---num_train_samples=5992411 \ 
+python3 train_on_tpu.py --tpu_worker "grpc://10.0.27.2:8470" \
+--class_labels="gs://shrill-anstett-us/C-MS-Celeb-class-labels.txt" \
+--dataset_path="gs://shrill-anstett-us/C-MS-Celeb-TFRecords" \
+--tensorboard_path="gs://shrill-anstett-us/C-MS-Celeb-FaceNet_TensorBoard" \
+--num_train_samples=5992411 \
 --num_val_samples=471607
